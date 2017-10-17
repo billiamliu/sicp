0f6b561d74ea7c1717d6bbcedd706c6c597b0db0
billiam <hello@billiam.io>
2017-10-17 14:00:43 -0700

    machine model

---
+++ register_machine/components/machine.scm
@@ -0,0 +1,67 @@
+(define (make-machine register-names ops controller-text)
+  (let ((machine (make-new-machine)))
+    (for-each (lambda (register-name)
+                ((machine 'allocate-register) register-name))
+              register-names)
+    ((machine 'install-operations) ops)
+    ((machine 'install-instruction-sequence)
+     (assemble controller-text machine))
+    machine))
+
+(define (start machine)
+  (machine 'start))
+
+(define (get-register-contents machine register-name)
+  (get-contents (get-register machine register-name)))
+
+(define (set-register-contents! machine register-name value)
+  (set-contents! (get-register machine register-name) value)
+  'done)
+
+(define (get-register machine reg-name)
+  ((machine 'get-register) reg-name))
+
+(define (make-new-machine)
+  (let ((pc (make-register 'pc)) ; program counter (where in the instruction set)
+        (flag (make-register 'flag)) ; for tests
+        (stack (make-stack))
+        (the-instruction-sequence '()))
+    (let ((the-ops
+            (list (list 'initialize-stack
+                        (lambda () (stack 'initialize)))))
+          (register-table
+            (list (list 'pc pc) (list 'flag flag))))
+      (define (allocate-register name)
+        (if (assoc name register-table)
+          (error "Multiply defined register: " name)
+          (set! register-table
+            (cons (list name (make-register name))
+                  register-table)))
+        'register-allocated)
+      (define (lookup-register name)
+        (let ((val (assoc name register-table)))
+          (if val
+            (cadr val)
+            (error "Unknown register:" name))))
+      (define (execute)
+        (let ((instructions (get-contents pc)))
+          (if (null? instructions)
+            'done
+            (begin
+              ((instruction-execution-proc (car insts)))
+              (execute)))))
+      (define (dispatch msg)
+        (cond ((eq? msg 'start)
+               (set-contents! pc the-instruction-sequence)
+               (execute))
+              ((eq? msg 'install-instruction-sequence)
+               (lambda (seq) (set! the-instruction-sequence seq)))
+              ((eq? msg 'allocate-register) allocate-register)
+              ((eq? msg 'get-register) lookup-register)
+              ((eq? msg 'install-operations)
+               (lambda (ops) (set! the-ops (append the-ops ops))))
+              ((eq? msg 'stack) stack)
+              ((eq? msg 'operations) the-ops)
+              (else (error "Unknown request -- MACHINE" msg))))
+      dispatch)))
+
