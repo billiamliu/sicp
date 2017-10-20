1f03de83ffa65fdfc68e89206606df02389a8dcf
billiam <hello@billiam.io>
2017-10-19 17:06:58 -0700

    EC Eval copied untested

--- register_machine/machine.scm
+++ register_machine/machine.scm
@@ -3,24 +3,3 @@
 (load "components/procedures")
 (load "components/register")
 (load "components/stack")
-
-
-
-(define (try a b)
-  (define gcd-machine
-    (make-machine
-      '(a b t)
-      (list (list 'rem remainder) (list '= =))
-      '(test-b
-         (test (op =) (reg b) (const 0))
-         (branch (label gcd-done))
-         (assign t (op rem) (reg a) (reg b))
-         (assign a (reg b))
-         (assign b (reg t))
-         (goto (label test-b))
-         gcd-done)))
-  (set-register-contents! gcd-machine 'a a)
-  (set-register-contents! gcd-machine 'b b)
-  (start gcd-machine)
-  ((gcd-machine 'stack) 'print-statistics)
-  (get-register-contents gcd-machine 'a))
